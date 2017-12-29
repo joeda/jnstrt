1b3e53cba3ae9dd4e66358e3afd955b39af4ecd6
joeda <johannes@walnutempire.de>
2017-12-29 14:21:21 +0100

    stuff

--- well_well_well/run.py
+++ well_well_well/run.py
@@ -17,33 +17,57 @@ square = [[ 1,  5, 27, 22, 28, 40, 14],
           [ 9, 23, 31, 29, 11, 36, 43]]
 N_COLS = len(square[0])
 N_ROWS = len(square)
-#data = np.matrix(square)
-G = nx.DiGraph()
+#data = np.matrix(square
 
-#set up nodes
-for r in range(N_ROWS):
-    for c in range(N_COLS):
-        G.add_node((r, c), depth=square[r][c])
+class Well:
+    def __init__(self):
+        self.time = 0
+        self.fr = 1 #flow rate
+        self.G = nx.DiGraph()
+        self.init_node = (0, 0)
+        # set up nodes
+        for r in range(N_ROWS):
+            for c in range(N_COLS):
+                self.G.add_node((r, c), depth=square[r][c])
 
-#vertical edges
-for r in range(N_ROWS - 1):
-    for c in range(N_COLS):
-        delta = G.nodes[(r+1,c)]["depth"] - G.nodes[(r, c)]["depth"]
-        if delta < 0:
-            G.add_edge((r, c), (r+1, c), grad= -delta)
+        # vertical edges
+        for r in range(N_ROWS - 1):
+            for c in range(N_COLS):
+                delta = self.G.nodes[(r + 1, c)]["depth"] - self.G.nodes[(r, c)]["depth"]
+                if delta > 0:
+                    self.G.add_edge((r, c), (r + 1, c), borders=1, capacity=delta)
+                else:
+                    self.G.add_edge((r + 1, c), (r, c), borders=1, capacity=-delta)
+
+        # horizontal edges
+        for r in range(N_ROWS):
+            for c in range(N_COLS - 1):
+                delta = self.G.nodes[(r, c + 1)]["depth"] - self.G.nodes[(r, c)]["depth"]
+                if delta > 0:
+                    self.G.add_edge((r, c), (r, c + 1), borders=1, capacity=delta)
+                else:
+                    self.G.add_edge((r, c + 1), (r, c), borders=1, capacity=-delta)
+
+    def isSink(self, node):
+        # check if node has successors
+        for _ in self.G.successors(node):
+            return False
+        maxFlow = nx.maximum_flow(self.G, self.init_node, node)
+        if maxFlow > 0:
+            return True
         else:
-            G.add_edge((r+1, c), (r, c), grad=delta)
+            return False
 
-#horizontal edges
-for r in range(N_ROWS):
-    for c in range(N_COLS - 1):
-        delta = G.nodes[(r, c+1)]["depth"] - G.nodes[(r, c)]["depth"]
-        if delta < 0:
-            G.add_edge((r, c), (r, c+1), grad= -delta)
-        else:
-            G.add_edge((r, c+1), (r, c), grad=delta)
-
-plt.subplot(121)
-nx.draw_spectral(G, with_labels=True, font_weight='bold')
-nx.draw_networkx_edge_labels(G)
-plt.show()
+    def mergeNodes(self, host, child):
+        child_only = []
+        both = []
+        for _ in self.G.successors(child):
+            raise RuntimeError("YOU FAIL")
+        for n in self.G.predecessors(child):
+            child_only.append(n)
+        child_only.remove(host)
+        for n in self.G.predecessors(host):
+            pass
+#plt.subplot(121)
+#nx.draw_spectral(G, with_labels=True, font_weight='bold')
+#plt.show()
